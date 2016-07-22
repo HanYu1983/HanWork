927bf8547b07b231f58cdcb30f31fe2ed5f7c791
HanYu <hanyushome@gmail.com>
2016-07-22 15:54:43 +0800

    no message

--- node/src/test.ls
+++ node/src/test.ls
@@ -53,61 +53,85 @@ readChunk = (filepath)->
       
       ..on "close", ->
         console.log "close"
-        
-parse = ({state, pos, buf}:ctx, obs)->
-  return switch state
-    | 0 =>
-      if pos >= 16
-        magic = buf.readUInt32BE()
-        dataLen = buf.readUInt32BE(4)
-        rows = buf.readUInt32BE(8)
-        columns = buf.readUInt32BE(12)
-        console.log "magic is #{magic}"
-        console.log "dataLen is #{dataLen}"
-        console.log "rows is #{rows}"
-        console.log "columns is #{columns}"
-        buf.copy buf, 0, 16, pos-16
-        [ 
-          true, 
-          ctx with do
-            state: 1
-            pos: pos-16
-            dataLen: dataLen
-            rows: rows
-            columns: columns
-        ]
-      else
-        [false]
-    | 1 =>
-      imagelen = ctx.rows* ctx.columns
-      if pos >= imagelen
-        imgbuf = new Buffer(imagelen)
-        buf.copy imgbuf, 0, 0, imagelen
-        obs.onNext(imgbuf)
-        buf.copy buf, 0, imagelen, pos - imagelen
-        [
-          true,
-          ctx with do
-            pos: pos - imagelen
-        ]
-      else
-        [false]
-    | otherwise =>
-      [false]
-        
-mnistData = (filepath)->
+      
+mnistDataLabel = (filepath)->
   Rx.Observable.create (obs)->
     readChunk(filepath).reduce do
-      ({state, pos, buf, imgidx}:ctx, chunk)->
+      ({state, pos, buf}:ctx, chunk)->
         chunk.copy(buf, pos)
         nctx = ctx with pos: pos + chunk.byteLength
-        parseWhileTrue = (ctx, obs)->
-          [ok, nctx] = parse ctx, obs
-          if ok
-            parseWhileTrue nctx, obs
-          else
-            ctx
-        parseWhileTrue nctx, obs
+        parse = ({state, pos, buf}:ctx)->
+          return switch state
+            | 0 =>
+              if pos >= 8
+                magic = buf.readUInt32BE()
+                num = buf.readUInt32BE(4)
+                console.log "magic)#{magic}"
+                console.log "num)#{num}"
+                buf.copy buf, 0, 8, pos - 8
+                parse ctx with pos: pos - 8, state: 1, num: num
+              else
+                ctx
+            | 1 =>
+              # 減少遞迴呼叫，因為遞迴深限有上限，大約8000次
+              for i from 0 til pos
+                label = buf.readUInt8(i)
+                obs.onNext label
+              # 這裡不需要搬移buf, 只要將curr position變為0就行了
+              # 直接回傳
+              ctx with pos: 0
+            | otherwise =>
+              ctx
+        parse nctx
+      {
+        state: 0
+        pos: 0
+        buf: new Buffer(65535*2)
+      }
+    .subscribe ->,->,->obs.onCompleted()
+        
+mnistDataImage = (filepath)->
+  Rx.Observable.create (obs)->
+    readChunk(filepath).reduce do
+      ({state, pos, buf}:ctx, chunk)->
+        chunk.copy(buf, pos)
+        nctx = ctx with pos: pos + chunk.byteLength
+        parse = ({state, pos, buf}:ctx)->
+          return switch state
+            | 0 =>
+              if pos >= 16
+                magic = buf.readUInt32BE()
+                dataLen = buf.readUInt32BE(4)
+                rows = buf.readUInt32BE(8)
+                columns = buf.readUInt32BE(12)
+                console.log "magic is #{magic}"
+                console.log "dataLen is #{dataLen}"
+                console.log "rows is #{rows}"
+                console.log "columns is #{columns}"
+                buf.copy buf, 0, 16, pos-16
+                parse ctx with do
+                  state: 1
+                  pos: pos - 16
+                  dataLen: dataLen
+                  rows: rows
+                  columns: columns
+              else
+                ctx
+            | 1 =>
+              imagelen = ctx.rows* ctx.columns
+              if pos >= imagelen
+                numofimg = Math.floor(pos/ imagelen)
+                for i from 0 til numofimg
+                  imgbuf = new Buffer(imagelen)
+                  buf.copy imgbuf, 0, imagelen* i, imagelen* i + imagelen
+                  obs.onNext(imgbuf)
+                buf.copy buf, 0, imagelen* numofimg , pos - imagelen* numofimg
+                ctx with pos: pos - imagelen* numofimg
+              else
+                ctx
+            | otherwise =>
+              ctx
+        parse nctx
       {
         state: 0
         pos: 0
@@ -115,11 +139,13 @@ mnistData = (filepath)->
       }
     .subscribe ->,->,->obs.onCompleted()
 
-mnistData("../doc/train-images-idx3-ubyte")
+
+mnistDataImage("../doc/train-images-idx3-ubyte")
   .zip do
     Rx.Observable.range(0, 60000)
     (data, idx)->
       [idx, data]
+  .skip(59990)
   .tapOnNext ([idx, data])->
     writeImage idx, 28, 28, data
       .subscribe ->,->,->
@@ -130,3 +156,19 @@ mnistData("../doc/train-images-idx3-ubyte")
     ->
       console.log "completed"
 
+/*
+
+mnistDataLabel("../doc/train-labels-idx1-ubyte")
+  .zip do
+    Rx.Observable.range(0, 60000)
+    (data, idx)->
+      [idx, data]
+  .skip(59990)
+  .subscribe do
+    ([idx, data])->
+      console.log "#{idx})#{data}"
+    (err)->
+      console.log err
+    ->
+      console.log "completed"
+*/
--- node/test.js
+++ node/test.js
@@ -1,4 +1,4 @@
-var fs, Canvas, Rx, reduce, global, writeImage, readChunk, parse, mnistData;
+var fs, Canvas, Rx, reduce, global, writeImage, readChunk, mnistDataLabel, mnistDataImage;
 fs = require('fs');
 Canvas = require('canvas');
 Rx = require('rx');
@@ -54,73 +54,44 @@ readChunk = function(filepath){
     return x$;
   });
 };
-parse = function(ctx, obs){
-  var state, pos, buf, magic, dataLen, rows, columns, imagelen, imgbuf;
-  state = ctx.state, pos = ctx.pos, buf = ctx.buf;
-  return (function(){
-    switch (state) {
-    case 0:
-      if (pos >= 16) {
-        magic = buf.readUInt32BE();
-        dataLen = buf.readUInt32BE(4);
-        rows = buf.readUInt32BE(8);
-        columns = buf.readUInt32BE(12);
-        console.log("magic is " + magic);
-        console.log("dataLen is " + dataLen);
-        console.log("rows is " + rows);
-        console.log("columns is " + columns);
-        buf.copy(buf, 0, 16, pos - 16);
-        return [
-          true, import$(clone$(ctx), {
-            state: 1,
-            pos: pos - 16,
-            dataLen: dataLen,
-            rows: rows,
-            columns: columns
-          })
-        ];
-      } else {
-        return [false];
-      }
-      break;
-    case 1:
-      imagelen = ctx.rows * ctx.columns;
-      if (pos >= imagelen) {
-        imgbuf = new Buffer(imagelen);
-        buf.copy(imgbuf, 0, 0, imagelen);
-        obs.onNext(imgbuf);
-        buf.copy(buf, 0, imagelen, pos - imagelen);
-        return [
-          true, import$(clone$(ctx), {
-            pos: pos - imagelen
-          })
-        ];
-      } else {
-        return [false];
-      }
-      break;
-    default:
-      return [false];
-    }
-  }());
-};
-mnistData = function(filepath){
+mnistDataLabel = function(filepath){
   return Rx.Observable.create(function(obs){
     return readChunk(filepath).reduce(function(ctx, chunk){
-      var state, pos, buf, imgidx, nctx, ref$, parseWhileTrue;
-      state = ctx.state, pos = ctx.pos, buf = ctx.buf, imgidx = ctx.imgidx;
+      var state, pos, buf, nctx, ref$, parse;
+      state = ctx.state, pos = ctx.pos, buf = ctx.buf;
       chunk.copy(buf, pos);
       nctx = (ref$ = clone$(ctx), ref$.pos = pos + chunk.byteLength, ref$);
-      parseWhileTrue = function(ctx, obs){
-        var ref$, ok, nctx;
-        ref$ = parse(ctx, obs), ok = ref$[0], nctx = ref$[1];
-        if (ok) {
-          return parseWhileTrue(nctx, obs);
-        } else {
-          return ctx;
-        }
+      parse = function(ctx){
+        var state, pos, buf, magic, num, i, label;
+        state = ctx.state, pos = ctx.pos, buf = ctx.buf;
+        return (function(){
+          var ref$, i$, to$;
+          switch (state) {
+          case 0:
+            if (pos >= 8) {
+              magic = buf.readUInt32BE();
+              num = buf.readUInt32BE(4);
+              console.log("magic)" + magic);
+              console.log("num)" + num);
+              buf.copy(buf, 0, 8, pos - 8);
+              return parse((ref$ = clone$(ctx), ref$.pos = pos - 8, ref$.state = 1, ref$.num = num, ref$));
+            } else {
+              return ctx;
+            }
+            break;
+          case 1:
+            for (i$ = 0, to$ = pos; i$ < to$; ++i$) {
+              i = i$;
+              label = buf.readUInt8(i);
+              obs.onNext(label);
+            }
+            return ref$ = clone$(ctx), ref$.pos = 0, ref$;
+          default:
+            return ctx;
+          }
+        }());
       };
-      return parseWhileTrue(nctx, obs);
+      return parse(nctx);
     }, {
       state: 0,
       pos: 0,
@@ -130,9 +101,75 @@ mnistData = function(filepath){
     });
   });
 };
-mnistData("../doc/train-images-idx3-ubyte").zip(Rx.Observable.range(0, 60000), function(data, idx){
+mnistDataImage = function(filepath){
+  return Rx.Observable.create(function(obs){
+    return readChunk(filepath).reduce(function(ctx, chunk){
+      var state, pos, buf, nctx, ref$, parse;
+      state = ctx.state, pos = ctx.pos, buf = ctx.buf;
+      chunk.copy(buf, pos);
+      nctx = (ref$ = clone$(ctx), ref$.pos = pos + chunk.byteLength, ref$);
+      parse = function(ctx){
+        var state, pos, buf, magic, dataLen, rows, columns, imagelen, numofimg, i, imgbuf;
+        state = ctx.state, pos = ctx.pos, buf = ctx.buf;
+        return (function(){
+          var i$, to$, ref$;
+          switch (state) {
+          case 0:
+            if (pos >= 16) {
+              magic = buf.readUInt32BE();
+              dataLen = buf.readUInt32BE(4);
+              rows = buf.readUInt32BE(8);
+              columns = buf.readUInt32BE(12);
+              console.log("magic is " + magic);
+              console.log("dataLen is " + dataLen);
+              console.log("rows is " + rows);
+              console.log("columns is " + columns);
+              buf.copy(buf, 0, 16, pos - 16);
+              return parse(import$(clone$(ctx), {
+                state: 1,
+                pos: pos - 16,
+                dataLen: dataLen,
+                rows: rows,
+                columns: columns
+              }));
+            } else {
+              return ctx;
+            }
+            break;
+          case 1:
+            imagelen = ctx.rows * ctx.columns;
+            if (pos >= imagelen) {
+              numofimg = Math.floor(pos / imagelen);
+              for (i$ = 0, to$ = numofimg; i$ < to$; ++i$) {
+                i = i$;
+                imgbuf = new Buffer(imagelen);
+                buf.copy(imgbuf, 0, imagelen * i, imagelen * i + imagelen);
+                obs.onNext(imgbuf);
+              }
+              buf.copy(buf, 0, imagelen * numofimg, pos - imagelen * numofimg);
+              return ref$ = clone$(ctx), ref$.pos = pos - imagelen * numofimg, ref$;
+            } else {
+              return ctx;
+            }
+            break;
+          default:
+            return ctx;
+          }
+        }());
+      };
+      return parse(nctx);
+    }, {
+      state: 0,
+      pos: 0,
+      buf: new Buffer(65535 * 2)
+    }).subscribe(function(){}, function(){}, function(){
+      return obs.onCompleted();
+    });
+  });
+};
+mnistDataImage("../doc/train-images-idx3-ubyte").zip(Rx.Observable.range(0, 60000), function(data, idx){
   return [idx, data];
-}).tapOnNext(function(arg$){
+}).skip(59990).tapOnNext(function(arg$){
   var idx, data;
   idx = arg$[0], data = arg$[1];
   return writeImage(idx, 28, 28, data).subscribe(function(){}, function(){}, function(){});
@@ -144,12 +181,28 @@ mnistData("../doc/train-images-idx3-ubyte").zip(Rx.Observable.range(0, 60000), f
 }, function(){
   return console.log("completed");
 });
+/*
+
+mnistDataLabel("../doc/train-labels-idx1-ubyte")
+  .zip do
+    Rx.Observable.range(0, 60000)
+    (data, idx)->
+      [idx, data]
+  .skip(59990)
+  .subscribe do
+    ([idx, data])->
+      console.log "#{idx})#{data}"
+    (err)->
+      console.log err
+    ->
+      console.log "completed"
+*/
+function clone$(it){
+  function fun(){} fun.prototype = it;
+  return new fun;
+}
 function import$(obj, src){
   var own = {}.hasOwnProperty;
   for (var key in src) if (own.call(src, key)) obj[key] = src[key];
   return obj;
-}
-function clone$(it){
-  function fun(){} fun.prototype = it;
-  return new fun;
 }
