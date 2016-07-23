085564cf9ac3a956ee8ae375c1b145830f6dca90
HanYu <hanyushome@gmail.com>
2016-07-23 14:33:43 +0800

    no message

--- node/src/test.ls
+++ node/src/test.ls
@@ -4,6 +4,7 @@ require! {
   rx: Rx
   ramda: {reduce, partial}
   convnetjs
+  "./tool.js": {readChunk, mnistDataLabel, mnistDataImage}
 }
 
 # 全域變數
@@ -45,105 +46,6 @@ writeImage = (imgidx, w, h, buf)->
           obs.onCompleted()
           
     writeImageClosure out, stream
-
-# 將檔案輸入流轉成Rx事件流
-readChunk = (filepath)->
-  Rx.Observable.create (obs)->
-    fs.createReadStream filepath
-      ..on "data", (chunk)->
-        obs.onNext chunk
-      
-      ..on "end", ->
-        obs.onCompleted()
-      
-      ..on "close", ->
-
-# 取得minst的label
-mnistDataLabel = (filepath)->
-  Rx.Observable.create (obs)->
-    readChunk(filepath).reduce do
-      ({state, pos, buf}:ctx, chunk)->
-        chunk.copy(buf, pos)
-        nctx = ctx with pos: pos + chunk.byteLength
-        parse = ({state, pos, buf}:ctx)->
-          return switch state
-            | 0 =>
-              if pos >= 8
-                magic = buf.readUInt32BE()
-                num = buf.readUInt32BE(4)
-                console.log "magic)#{magic}"
-                console.log "num)#{num}"
-                buf.copy buf, 0, 8, pos - 8
-                parse ctx with pos: pos - 8, state: 1, num: num
-              else
-                ctx
-            | 1 =>
-              # 減少遞迴呼叫，因為遞迴深限有上限，大約8000次
-              for i from 0 til pos
-                label = buf.readUInt8(i)
-                obs.onNext label
-              # 這裡不需要搬移buf, 只要將curr position變為0就行了
-              # 直接回傳
-              ctx with pos: 0
-            | otherwise =>
-              ctx
-        parse nctx
-      {
-        state: 0
-        pos: 0
-        buf: new Buffer(65535*2)
-      }
-    .subscribe ->,->,->obs.onCompleted()
-
-# 取得mnist的圖片
-mnistDataImage = (filepath)->
-  Rx.Observable.create (obs)->
-    readChunk(filepath).reduce do
-      ({state, pos, buf}:ctx, chunk)->
-        chunk.copy(buf, pos)
-        nctx = ctx with pos: pos + chunk.byteLength
-        parse = ({state, pos, buf}:ctx)->
-          return switch state
-            | 0 =>
-              if pos >= 16
-                magic = buf.readUInt32BE()
-                dataLen = buf.readUInt32BE(4)
-                rows = buf.readUInt32BE(8)
-                columns = buf.readUInt32BE(12)
-                console.log "magic is #{magic}"
-                console.log "dataLen is #{dataLen}"
-                console.log "rows is #{rows}"
-                console.log "columns is #{columns}"
-                buf.copy buf, 0, 16, pos-16
-                parse ctx with do
-                  state: 1
-                  pos: pos - 16
-                  dataLen: dataLen
-                  rows: rows
-                  columns: columns
-              else
-                ctx
-            | 1 =>
-              imagelen = ctx.rows* ctx.columns
-              if pos >= imagelen
-                numofimg = Math.floor(pos/ imagelen)
-                for i from 0 til numofimg
-                  imgbuf = new Buffer(imagelen)
-                  buf.copy imgbuf, 0, imagelen* i, imagelen* i + imagelen
-                  obs.onNext(imgbuf)
-                buf.copy buf, 0, imagelen* numofimg , pos - imagelen* numofimg
-                ctx with pos: pos - imagelen* numofimg
-              else
-                ctx
-            | otherwise =>
-              ctx
-        parse nctx
-      {
-        state: 0
-        pos: 0
-        buf: new Buffer(65535*2)
-      }
-    .subscribe ->,->,->obs.onCompleted()
 
 testConvnet = ->
   layer_defs =
--- node/test.js
+++ node/test.js
@@ -1,9 +1,10 @@
-var fs, Canvas, Rx, ref$, reduce, partial, convnetjs, global, writeImage, readChunk, mnistDataLabel, mnistDataImage, testConvnet, testWriteImage, testShowLabel;
+var fs, Canvas, Rx, ref$, reduce, partial, convnetjs, readChunk, mnistDataLabel, mnistDataImage, global, writeImage, testConvnet, testWriteImage, testShowLabel;
 fs = require('fs');
 Canvas = require('canvas');
 Rx = require('rx');
 ref$ = require('ramda'), reduce = ref$.reduce, partial = ref$.partial;
 convnetjs = require('convnetjs');
+ref$ = require('./tool.js'), readChunk = ref$.readChunk, mnistDataLabel = ref$.mnistDataLabel, mnistDataImage = ref$.mnistDataImage;
 global = {
   imgdir: "../output"
 };
@@ -36,133 +37,6 @@ writeImage = function(imgidx, w, h, buf){
       });
     };
     return writeImageClosure(out, stream);
-  });
-};
-readChunk = function(filepath){
-  return Rx.Observable.create(function(obs){
-    var x$;
-    x$ = fs.createReadStream(filepath);
-    x$.on("data", function(chunk){
-      return obs.onNext(chunk);
-    });
-    x$.on("end", function(){
-      return obs.onCompleted();
-    });
-    x$.on("close", function(){});
-    return x$;
-  });
-};
-mnistDataLabel = function(filepath){
-  return Rx.Observable.create(function(obs){
-    return readChunk(filepath).reduce(function(ctx, chunk){
-      var state, pos, buf, nctx, ref$, parse;
-      state = ctx.state, pos = ctx.pos, buf = ctx.buf;
-      chunk.copy(buf, pos);
-      nctx = (ref$ = clone$(ctx), ref$.pos = pos + chunk.byteLength, ref$);
-      parse = function(ctx){
-        var state, pos, buf, magic, num, i, label;
-        state = ctx.state, pos = ctx.pos, buf = ctx.buf;
-        return (function(){
-          var ref$, i$, to$;
-          switch (state) {
-          case 0:
-            if (pos >= 8) {
-              magic = buf.readUInt32BE();
-              num = buf.readUInt32BE(4);
-              console.log("magic)" + magic);
-              console.log("num)" + num);
-              buf.copy(buf, 0, 8, pos - 8);
-              return parse((ref$ = clone$(ctx), ref$.pos = pos - 8, ref$.state = 1, ref$.num = num, ref$));
-            } else {
-              return ctx;
-            }
-            break;
-          case 1:
-            for (i$ = 0, to$ = pos; i$ < to$; ++i$) {
-              i = i$;
-              label = buf.readUInt8(i);
-              obs.onNext(label);
-            }
-            return ref$ = clone$(ctx), ref$.pos = 0, ref$;
-          default:
-            return ctx;
-          }
-        }());
-      };
-      return parse(nctx);
-    }, {
-      state: 0,
-      pos: 0,
-      buf: new Buffer(65535 * 2)
-    }).subscribe(function(){}, function(){}, function(){
-      return obs.onCompleted();
-    });
-  });
-};
-mnistDataImage = function(filepath){
-  return Rx.Observable.create(function(obs){
-    return readChunk(filepath).reduce(function(ctx, chunk){
-      var state, pos, buf, nctx, ref$, parse;
-      state = ctx.state, pos = ctx.pos, buf = ctx.buf;
-      chunk.copy(buf, pos);
-      nctx = (ref$ = clone$(ctx), ref$.pos = pos + chunk.byteLength, ref$);
-      parse = function(ctx){
-        var state, pos, buf, magic, dataLen, rows, columns, imagelen, numofimg, i, imgbuf;
-        state = ctx.state, pos = ctx.pos, buf = ctx.buf;
-        return (function(){
-          var i$, to$, ref$;
-          switch (state) {
-          case 0:
-            if (pos >= 16) {
-              magic = buf.readUInt32BE();
-              dataLen = buf.readUInt32BE(4);
-              rows = buf.readUInt32BE(8);
-              columns = buf.readUInt32BE(12);
-              console.log("magic is " + magic);
-              console.log("dataLen is " + dataLen);
-              console.log("rows is " + rows);
-              console.log("columns is " + columns);
-              buf.copy(buf, 0, 16, pos - 16);
-              return parse(import$(clone$(ctx), {
-                state: 1,
-                pos: pos - 16,
-                dataLen: dataLen,
-                rows: rows,
-                columns: columns
-              }));
-            } else {
-              return ctx;
-            }
-            break;
-          case 1:
-            imagelen = ctx.rows * ctx.columns;
-            if (pos >= imagelen) {
-              numofimg = Math.floor(pos / imagelen);
-              for (i$ = 0, to$ = numofimg; i$ < to$; ++i$) {
-                i = i$;
-                imgbuf = new Buffer(imagelen);
-                buf.copy(imgbuf, 0, imagelen * i, imagelen * i + imagelen);
-                obs.onNext(imgbuf);
-              }
-              buf.copy(buf, 0, imagelen * numofimg, pos - imagelen * numofimg);
-              return ref$ = clone$(ctx), ref$.pos = pos - imagelen * numofimg, ref$;
-            } else {
-              return ctx;
-            }
-            break;
-          default:
-            return ctx;
-          }
-        }());
-      };
-      return parse(nctx);
-    }, {
-      state: 0,
-      pos: 0,
-      buf: new Buffer(65535 * 2)
-    }).subscribe(function(){}, function(){}, function(){
-      return obs.onCompleted();
-    });
   });
 };
 testConvnet = function(){
@@ -288,12 +162,3 @@ testShowLabel = function(){
   });
 };
 testConvnet();
-function clone$(it){
-  function fun(){} fun.prototype = it;
-  return new fun;
-}
-function import$(obj, src){
-  var own = {}.hasOwnProperty;
-  for (var key in src) if (own.call(src, key)) obj[key] = src[key];
-  return obj;
-}
