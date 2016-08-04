e2ed3ccfc7df3fab715b5aac55825d103ed078c6
HanYu <hanyushome@gmail.com>
2016-08-04 14:26:26 +0800

    no message

--- dev/node/src/web/bw.ls
+++ dev/node/src/web/bw.ls
@@ -41,14 +41,25 @@ ChangePlayer = (player, model)->
         1
       else
         0
+        
+MoveCursor = (t, model)->
+  merge do
+    model
+    cursor:
+      map (([a,b])->a+b), zip(t, model.cursor)
 
 ApplyBlackWhiteTransform = (posId, model)->
   model
+  
+SendEvent = (cmd, obj)->
+  window.global.onView.onNext [cmd, obj]
+  obj
 
 Main = ->
   model = 
     board: repeat(EMPTY, W* H)
     currplayer: 0
+    cursor: [0,0]
   
   onKeyUp =
     document.body |>
@@ -60,24 +71,48 @@ Main = ->
         .map ({code})->
           ['keyup', code]
       window.global.onModel
+      Rx.Observable.just(['init'])
     
   onInput.reduce do
     (model, [cmd, params])->
       console.log cmd, params
       console.log model
       switch cmd
-        | "keyup" =>
-          ChangePlayerIfSuccessPut = (oldmodel, newmodel)->
-            if equals(oldmodel.board, newmodel.board)
-              oldmodel
-            else
-              ChangePlayer newmodel.currplayer, newmodel
-          piece = PlayerPiece model.currplayer
-          posId = PosId [0,0]
+        | "init" =>
+          SendEvent ["init", model]
           model
-            |> (PutPiece piece, posId, _) 
-            |> (ChangePlayerIfSuccessPut model, _)
-            |> (ApplyBlackWhiteTransform posId, _)
+        | "keyup" =>
+          switch params
+            | "ArrowUp","ArrowDown","ArrowLeft","ArrowRight" =>
+              MoveCursor do
+                switch params
+                  | "ArrowUp" => [0, -1]
+                  | "ArrowDown" => [0, 1]
+                  | "ArrowLeft" => [-1, 0]
+                  | "ArrowRight" => [1, 0]
+                model
+              |> (SendEvent "updateCursor", _)
+            | "Space" =>
+              ChangePlayerIfSuccessPut = (oldmodel, newmodel)->
+                # 如果棋盤沒有變，代表沒有下成功，不需改變玩家
+                if equals(oldmodel.board, newmodel.board)
+                  oldmodel
+                else
+                  ChangePlayer newmodel.currplayer, newmodel
+              SendUpdateEventIfSuccessPut = (oldmodel, newmodel)->
+                if equals(oldmodel.board, newmodel.board)
+                  oldmodel
+                else
+                  SendEvent "update", newmodel
+              piece = PlayerPiece model.currplayer
+              posId = PosId model.cursor
+              model
+                |> (PutPiece piece, posId, _) 
+                |> (ChangePlayerIfSuccessPut model, _)
+                |> (ApplyBlackWhiteTransform posId, _)
+                |> (SendUpdateEventIfSuccessPut model, _)
+            | otherwise =>
+              model
         | otherwise => 
           model
     model
--- dev/www/build/js/bw.js
+++ dev/www/build/js/bw.js
@@ -8785,7 +8785,7 @@
 }.call(this));
 
 },{}],2:[function(require,module,exports){
-var ref$, repeat, map, zip, merge, equals, W, H, EMPTY, WHITE, BLACK, PosId, PlayerPiece, PutPiece, ChangePlayer, ApplyBlackWhiteTransform, Main, slice$ = [].slice;
+var ref$, repeat, map, zip, merge, equals, W, H, EMPTY, WHITE, BLACK, PosId, PlayerPiece, PutPiece, ChangePlayer, MoveCursor, ApplyBlackWhiteTransform, SendEvent, Main, slice$ = [].slice;
 ref$ = require('ramda'), repeat = ref$.repeat, map = ref$.map, zip = ref$.zip, merge = ref$.merge, equals = ref$.equals;
 W = 8;
 H = 8;
@@ -8832,41 +8832,89 @@ ChangePlayer = function(player, model){
     currplayer: player === 0 ? 1 : 0
   });
 };
+MoveCursor = function(t, model){
+  return merge(model, {
+    cursor: map(function(arg$){
+      var a, b;
+      a = arg$[0], b = arg$[1];
+      return a + b;
+    }, zip(t, model.cursor))
+  });
+};
 ApplyBlackWhiteTransform = function(posId, model){
   return model;
+};
+SendEvent = function(cmd, obj){
+  window.global.onView.onNext([cmd, obj]);
+  return obj;
 };
 Main = function(){
   var model, onKeyUp, onInput;
   model = {
     board: repeat(EMPTY, W * H),
-    currplayer: 0
+    currplayer: 0,
+    cursor: [0, 0]
   };
   onKeyUp = Rx.Observable.fromEvent(document.body, "keyup");
   onInput = Rx.Observable.merge(onKeyUp.map(function(arg$){
     var code;
     code = arg$.code;
     return ['keyup', code];
-  }), window.global.onModel);
+  }), window.global.onModel, Rx.Observable.just(['init']));
   return onInput.reduce(function(model, arg$){
-    var cmd, params, ChangePlayerIfSuccessPut, piece, posId;
+    var cmd, params, ChangePlayerIfSuccessPut, SendUpdateEventIfSuccessPut, piece, posId;
     cmd = arg$[0], params = arg$[1];
     console.log(cmd, params);
     console.log(model);
     switch (cmd) {
+    case "init":
+      SendEvent(["init", model]);
+      return model;
     case "keyup":
-      ChangePlayerIfSuccessPut = function(oldmodel, newmodel){
-        if (equals(oldmodel.board, newmodel.board)) {
-          return oldmodel;
-        } else {
-          return ChangePlayer(newmodel.currplayer, newmodel);
-        }
-      };
-      piece = PlayerPiece(model.currplayer);
-      posId = PosId([0, 0]);
-      return partialize$.apply(this, [ApplyBlackWhiteTransform, [posId, void 8], [1]])(
-      partialize$.apply(this, [ChangePlayerIfSuccessPut, [model, void 8], [1]])(
-      partialize$.apply(this, [PutPiece, [piece, posId, void 8], [2]])(
-      model)));
+      switch (params) {
+      case "ArrowUp":
+      case "ArrowDown":
+      case "ArrowLeft":
+      case "ArrowRight":
+        return partialize$.apply(this, [SendEvent, ["updateCursor", void 8], [1]])(
+        MoveCursor((function(){
+          switch (params) {
+          case "ArrowUp":
+            return [0, -1];
+          case "ArrowDown":
+            return [0, 1];
+          case "ArrowLeft":
+            return [-1, 0];
+          case "ArrowRight":
+            return [1, 0];
+          }
+        }()), model));
+      case "Space":
+        ChangePlayerIfSuccessPut = function(oldmodel, newmodel){
+          if (equals(oldmodel.board, newmodel.board)) {
+            return oldmodel;
+          } else {
+            return ChangePlayer(newmodel.currplayer, newmodel);
+          }
+        };
+        SendUpdateEventIfSuccessPut = function(oldmodel, newmodel){
+          if (equals(oldmodel.board, newmodel.board)) {
+            return oldmodel;
+          } else {
+            return SendEvent("update", newmodel);
+          }
+        };
+        piece = PlayerPiece(model.currplayer);
+        posId = PosId(model.cursor);
+        return partialize$.apply(this, [SendUpdateEventIfSuccessPut, [model, void 8], [1]])(
+        partialize$.apply(this, [ApplyBlackWhiteTransform, [posId, void 8], [1]])(
+        partialize$.apply(this, [ChangePlayerIfSuccessPut, [model, void 8], [1]])(
+        partialize$.apply(this, [PutPiece, [piece, posId, void 8], [2]])(
+        model))));
+      default:
+        return model;
+      }
+      break;
     default:
       return model;
     }
