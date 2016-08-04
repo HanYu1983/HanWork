4bfc02978122af3b969bc0c6f4f7f73aea76cf1c
vic <finalsteve@hotmail.com>
2016-08-04 13:53:37 +0800

    update

--- dev/www/bw/js/vic.view.js
+++ dev/www/bw/js/vic.view.js
@@ -11,8 +11,8 @@ vic.view = vic.view || {};
 	
 	var plate;
 	var select;
-	var mats, objl;
 	var iconCon;
+	var ary_icons;
 	
 	var icon_ori_pos = new THREE.Vector3();
 	icon_ori_pos.x = -8.7;
@@ -29,14 +29,12 @@ vic.view = vic.view || {};
 		ary_icon_pos[x+'_'+y] = pos;
 	}
 	
-	console.log( ary_icon_pos );
-	
 	function getPositionByPos(x, y){
 		return ary_icon_pos[x+'_'+y];
 	}
 	
-	function prepareLib( ary, cb ){
-		var tempConfig = ary.slice();
+	function prepareLib( cb ){
+		var tempConfig = vic.view.meshConfig.slice();
 		loadOne();
 		
 		function loadOne(){
@@ -56,9 +54,15 @@ vic.view = vic.view || {};
 					});
 				}
 			}else{
-				cb( ary );
+				cb( vic.view.meshConfig );
 			}
 		}
+	}
+	
+	function startGame(){
+		prepareLib( function( lib ){
+			init( lib, $('#webgl') );
+		});
 	}
 	
 	function cloneMeshFromLib( lib, name ){
@@ -94,13 +98,63 @@ vic.view = vic.view || {};
 		scene.add( mesh );
 		mesh.position.x = 10;
 		mesh.position.y = 10;
+		mesh.extra = {
+			pos:{x:x, y:y},
+			black:black
+		}
 		
 		Tweener.addTween(mesh.position, {x:toGlobal.x, y:toGlobal.y, z:toGlobal.z, time:1, transition:"easeOutQuad"});
 		if( black )
 			Tweener.addTween(mesh.rotation, {x:3.14, time:1, transition:"easeOutQuad"});
+		
+		ary_icons.push( mesh );
+	}
+	
+	function flipIcon(x, y, black ){
+		var icon = getIcon(x, y );
+		if( icon != undefined ){
+			if( black && !icon.extra.black ){
+				Tweener.addTween(icon.rotation, {x:3.14, time:1, transition:"easeOutQuad"});
+			}else if( !black && icon.extra.black ){
+				Tweener.addTween(icon.rotation, {x:0, time:1, transition:"easeOutQuad"});
+			}
+			icon.extra.black = black;
+		}
+	}
+	
+	function deleteIcon(x, y){
+		var icon = getIcon(x,y);
+		if( icon != undefined ){
+			scene.remove( icon );
+			var id = ary_icons.indexOf( icon );
+			ary_icons.splice( id, 1 );
+		}
+	}
+	
+	function getIcon(x, y){
+		for( var i = 0; i < ary_icons.length; ++i ){
+			var icon = ary_icons[i];
+			if( icon.extra.pos.x == x && icon.extra.pos.y == y ){
+				return icon;
+			}
+		}
+		return undefined;
+	}
+	
+	function clearIcon(){
+		_.each( ary_icons, function( icon ){
+			scene.remove( icon );
+		});
+		ary_icons = [];
+	}
+	
+	function syncView( model ){
+		console.log( model );
 	}
 
 	function init( lib, div_container ) {
+		
+		ary_icons = [];
 		
 		camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
 		camera.position.y = 50;
@@ -111,9 +165,6 @@ vic.view = vic.view || {};
 		iconCon.position.y = icon_ori_pos.y;
 		iconCon.position.z = icon_ori_pos.z;
 		
-		console.log( iconCon.matrixWorld );
-		
-
 		scene = new THREE.Scene();
 		scene.add( iconCon );
 		
@@ -135,24 +186,48 @@ vic.view = vic.view || {};
 			scene.add( select );
 		select.position.y = 0;
 		
-		/*
-		for( var i = 0; i < 64; ++i ){
-			var icon = cloneMeshFromLib( lib, 'icon' );
-			if( icon != undefined ){
-				var pos = getPositionByPos( i % 8, Math.floor( i / 8 ) );
-				
-				icon.position.x = pos[0];
-				icon.position.z = pos[1];
-				iconCon.add( icon );
-				Tweener.addTween(icon.rotation, {x:3.14, time:2, delay:i * 1, transition:"easeOutBounce"});
-			}
-		}
-		*/
 		putSelect(1, 1 );
 		putIcon(2, 3, true, cloneMeshFromLib( lib, 'icon' ) );
 		putIcon(3, 6, true, cloneMeshFromLib( lib, 'icon' ) );
 		putIcon(4, 1, false, cloneMeshFromLib( lib, 'icon' ) );
 		putIcon(2, 6, true, cloneMeshFromLib( lib, 'icon' ) );
+		putIcon(3, 5, true, cloneMeshFromLib( lib, 'icon' ) );
+		
+		_.delay( function(){
+			flipIcon( 3, 6, false );
+			flipIcon( 3, 5, false );
+			flipIcon( 2, 6, false );
+			flipIcon( 2, 3, true );
+			flipIcon( 3, 6, false );
+		}, 2000 );
+		
+		_.delay( function(){
+			deleteIcon( 3, 6 );
+		}, 4000 );
+		
+		_.delay( function(){
+			flipIcon( 3, 6, true );
+			flipIcon( 2, 6, true );
+			deleteIcon( 3, 5 );
+		}, 6000 );
+		
+		_.delay( function(){
+			clearIcon();
+		}, 8000 );
+		
+		_.delay( function(){
+			putIcon(2, 3, true, cloneMeshFromLib( lib, 'icon' ) );
+			putIcon(3, 6, true, cloneMeshFromLib( lib, 'icon' ) );
+			putIcon(4, 1, false, cloneMeshFromLib( lib, 'icon' ) );
+		}, 10000 );
+		
+		_.delay( function(){
+			flipIcon( 3, 6, false );
+			flipIcon( 3, 5, false );
+			flipIcon( 4, 1, true );
+			flipIcon( 2, 3, true );
+			flipIcon( 3, 6, false );
+		}, 12000 );
 		
 
 		renderer = new THREE.WebGLRenderer();
@@ -163,7 +238,7 @@ vic.view = vic.view || {};
 		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 		window.addEventListener( 'resize', onWindowResize, false );
-
+		animate();
 	}
 
 	function onWindowResize() {
@@ -202,24 +277,13 @@ vic.view = vic.view || {};
 		//camera.lookAt( scene.position );
 		
 		select.rotation.y += .01;
-
 		renderer.render( scene, camera );
 
 	}
 	
-	module.prepareLib = prepareLib;
-	module.init = init;
+	module.startGame = startGame;
 	module.animate = animate;
+	module.syncView = syncView;
 })( vic.view );
 
-vic.view.meshConfig = [
-						{name:'plate', path:'asset/plate.obj',mtlPath:'asset/plate.mtl'}, 
-						{name:'icon', path:'asset/icon.obj', mtlPath:'asset/icon.mtl'},
-						{name:'border', path:'asset/border.obj'}
-						];
-						
-vic.view.prepareLib( vic.view.meshConfig, function( lib ){
-	
-	vic.view.init( lib, $('#webgl') );
-	vic.view.animate();
-});
+vic.view.startGame();
