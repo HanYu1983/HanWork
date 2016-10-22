436b3c856f00bef38d075b30dac82a1b03997aa7
vic <finalsteve@hotmail.com>
2016-10-22 16:03:34 +0800

    update

--- dev/www/sgs_online/js/vic.config.js
+++
@@ -1,7 +0,0 @@
-var vic = vic || {};
-vic.view = vic.view || {};
-vic.view.meshConfig = [
-						{name:'plate', path:'asset/plate.obj',mtlPath:'asset/plate.mtl'}, 
-						{name:'icon', path:'asset/icon.obj', mtlPath:'asset/icon.mtl'},
-						{name:'border', path:'asset/border.obj'}
-						];
