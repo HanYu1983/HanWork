00380b2f9967bdad266f42991447cc1d46fea3d8
HanYu <hanyushome@gmail.com>
2019-01-25 23:06:07 +0800

    no message

--- funcgame/g1/src/app/t1/main.cljs
+++ funcgame/g1/src/app/t1/main.cljs
@@ -45,9 +45,8 @@
                   :leftCircle [[10 50 30 :fish] [10 100 30 :family] [10 150 30 :rock]]
                   :rightCircle [[190 50 30 :rock] [190 100 30 :fish] [190 150 30 :family]]
                   :line nil
-                  :state :select1
                   :select1 nil
-                  :buildLine []}]
+                  :buildLine #{}}]
   (set! model ctx)
   (let [e (a/<! evt)]
     (condp = (:type e)
@@ -90,7 +89,7 @@
                    (map vector (range))
                    (filter (fn [[_ selected]] selected))
                    (ffirst))]
-      (merge ctx {:mouseDown false :select1 idx :state :select2}))
+      (merge ctx {:mouseDown false :select1 idx}))
     
     (:mouseUp ctx)
     (let [idx (->>
@@ -99,13 +98,13 @@
                    (filter (fn [[_ selected]] selected))
                    (ffirst))]
       (if-not idx
-        (merge ctx {:mouseUp false :select1 nil :state :select1})
+        (merge ctx {:mouseUp false :select1 nil})
         (let [s1 (:select1 ctx)
               s2 idx
               pass (checkAnswer ctx s1 s2)]
           (if-not pass
-            (merge ctx {:mouseUp false :select1 nil :state :select1})
-            (merge ctx {:mouseUp false :select1 nil :buildLine (conj (:buildLine ctx) [s1 s2]) :state :check})))))
+            (merge ctx {:mouseUp false :select1 nil})
+            (merge ctx {:mouseUp false :select1 nil :buildLine (conj (:buildLine ctx) [s1 s2])})))))
     
     :else
     ctx))
@@ -125,13 +124,14 @@
       ;(.log js/console (clj->js model))
       (if-not model
         (.text pc "no model" 30 30)
-        (do
+        (let [mousePos (:mousePos model)
+              [mx my] mousePos]
           (.background pc 102)
           (.fill pc 120)
-          (doall 
+          (dorun 
             (doseq [c (concat (:leftCircle model) (:rightCircle model))]
               (let [[x y radius] c
-                    selected (isPointInCircle (:mousePos model) c)]
+                    selected (isPointInCircle mousePos c)]
                 (if-not selected
                   (do
                     (.fill pc 255)
@@ -140,17 +140,16 @@
                     (.fill pc 100 0 0)
                     (.ellipse pc x y radius radius))))))
           (if-let [s1 (:select1 model)]
-            (let [[x y] (get-in model [:leftCircle s1])
-                  [mx my] (:mousePos model)]
+            (let [[x y] (get-in model [:leftCircle s1])]
               (.line pc x y mx my)))
-          (doall
+          (dorun
             (doseq [line (:buildLine model)]
               (let [[s1 s2] line
                     [x y] (nth (:leftCircle model) s1)
                     [x2 y2] (nth (:rightCircle model) s2)]
                 (.line pc x y x2 y2))))
           (.fill pc 255)
-          (.ellipse pc (nth (:mousePos model) 0) (nth (:mousePos model) 1) 10 10))))))
+          (.ellipse pc mx my 10 10))))))
 
 
 (let [canvas (.getElementById js/document "canvas1")
