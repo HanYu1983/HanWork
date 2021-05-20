be2d8b912f95da8003e3c1e45b23b75dd135ce50
HanYu1983 <hanyushome@gmail.com>
2021-05-20 20:12:42 +0800

    add react test

---
+++ react/app1/src/App.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { map } from "rxjs/operators"
+import { bind, Subscribe  } from "@react-rxjs/core"
+import { createSignal } from "@react-rxjs/utils"
+
+// A signal is an entry point to react-rxjs. It's equivalent to using a subject
+const [textChange$, setText] = createSignal<string>();
+
+const [useText, text$] = bind<string>(textChange$, "")
+
+function TextInput() {
+  const text = useText()
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      Echo: {text}
+    </div>
+  )
+}
+
+const [useCharCount, charCount$] = bind(
+  text$.pipe(
+    map((text) => text.length)
+  )
+)
+
+function CharacterCount() {
+  const count = useCharCount()
+  return <>Character Count: {count}</>
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <Subscribe>
+        <TextInput />
+        <CharacterCount />
+      </Subscribe>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <TextInput></TextInput>
+        <CharacterCounter></CharacterCounter>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
